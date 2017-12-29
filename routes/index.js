55dc4b45981a1bf3f8441d8874d7d81e8a1611a4
lmajam <majamlemuel@gmail.com>
2017-12-29 21:32:09 +0800

    contact form

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const app = express();
 app.use('/user', require('./user'));
 app.use('/listings', require('./listings.route'));
 app.use('/icons', require('./icons.route'));
+app.use('/inquiries', require('./inquiries.route'));
 
 module.exports = app;
