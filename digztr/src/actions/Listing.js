5136ed57b838656139ec2d513dd05b8de381f44b
lmajam <majamlemuel@gmail.com>
2018-01-08 14:32:28 +0800

    updated react route to fetch listing by id

--- digztr/src/actions/Listing.js
+++ digztr/src/actions/Listing.js
@@ -29,15 +29,53 @@ export const _loadResponse = (response) => ({
  * @param userID user id
  * @returns {Function}
  */
- export function load() {
-   return (dispatch, getState) => {
-     dispatch(_loadRequest());
-     axios.get(`${config.api.baseUrl}/api/listings`)
-      .then(res => {
-        dispatch(_loadResponse(res.data[0]));
-      });
-   }
+export function load() {
+ return (dispatch, getState) => {
+   dispatch(_loadRequest());
+   axios.get(`${config.api.baseUrl}/api/listings`)
+    .then(res => {
+      dispatch(_loadResponse(res.data));
+    });
  }
+}
+
+/**
+ * LOAD
+ * @type {string}
+ */
+export const LOAD_BY_ID = 'LISTING_LOAD_BY_ID';
+
+/**
+ * _loadRequest
+ * @private
+ */
+export const _loadByIdRequest = () => ({ type: LOAD });
+
+/**
+ * _loadResponse
+ * @param response
+ * @private
+ */
+export const _loadByIdResponse = (response) => ({
+	type: LOAD,
+	response,
+});
+
+/**
+ * load by ID
+ * Gets profile data from the API for a given user id
+ * @param userID user id
+ * @returns {Function}
+ */
+export function loadById(id) {
+ return (dispatch, getState) => {
+   dispatch(_loadByIdRequest());
+   axios.get(`${config.api.baseUrl}/api/listings/${id}`)
+    .then(res => {
+      dispatch(_loadByIdResponse(res.data));
+    });
+ }
+}
 
  /**
   * LOAD
--- digztr/src/routes/ListingDetails/index.js
+++ digztr/src/routes/ListingDetails/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  path: '/',
+  path: '/:id',
 
   /**
      * getComponent
